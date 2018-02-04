331be3578adb737360a2b9d08570e593d63d420b
Vanessa Ingram <vingram15@gmail.com>
2018-02-03 20:06:54 -0800

    commit portfolio code

---
+++ example/Portfolio - Jack Tomaszewski - full-stack freelance web developer_files/main.66464428.js
@@ -0,0 +1,1 @@
+function onReady(){var e=document.querySelector(".main-navigation");document.querySelector(".js-extend-main-navigation").addEventListener("click",function(n){n.preventDefault(),e.classList.toggle("extended")},!1)}document.addEventListener("DOMContentLoaded",onReady);
